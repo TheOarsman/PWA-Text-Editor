9d3b4160113963a4bbb6c833b184440c5e71b739
TheOarsman <heinzulrichv@gmail.com>
2024-02-29 08:00:52 -0500

    adjustments made

--- client/src-sw.js
+++ client/src-sw.js
@@ -19,8 +19,8 @@ const pageCache = new CacheFirst({
   ],
 });
 
-// Attaching offlineFallback to pageCache
-offlineFallback({ strategy: pageCache });
+// // Attaching offlineFallback to pageCache
+// offlineFallback({ strategy: pageCache });
 
 warmStrategyCache({
   urls: ["/index.html", "/"],
@@ -31,15 +31,15 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // Implements asset caching
 registerRoute(
-  /\.(?:js|css|png|jpg|jpeg|svg|gif)$/,
-  new CacheFirst({
-    cacheName: "assets-cache",
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: "asset-cache",
     plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for 7 days
       }),
     ],
   })
